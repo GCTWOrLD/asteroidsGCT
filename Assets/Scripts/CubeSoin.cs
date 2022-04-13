2784ac9bf2f0222893b4c01a7c69ed009aad526f
GCTWOrLD <gabriel.caron.tawil@gmail.com>
2022-04-13 17:35:00 -0400

    Cube de soin qui va éventuellement redonner de la vie au joueur

---
+++ Assets/Scripts/CubeSoin.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSoin : MonoBehaviour
+{
+    private Vector3 rotation, translation;
+    public float rotationSpeed = 60f;
+    public float movementSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        translation = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotation, rotationSpeed * Time.deltaTime);
+        transform.position += translation * Time.deltaTime; //equivalent à transform.Translate(translation * Time.deltaTime, Space.World);
+
+        if (Mathf.Abs(transform.position.x) > 9f)
+        {
+            var newPos = transform.position;
+            newPos.x = -transform.position.x;
+            transform.position = newPos;
+        }
+        if (Mathf.Abs(transform.position.y) > 5f)
+        {
+            var newPos = transform.position;
+            newPos.y = -transform.position.y;
+            transform.position = newPos;
+        }
+    }
+}
