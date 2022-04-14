a1f8fa5c303e9233da873f76999c97425ac8fb05
GCTWOrLD <gabriel.caron.tawil@gmail.com>
2022-04-14 17:29:23 -0400

    Ajout de conditions de destruction des aliens

--- Assets/Scripts/VaisseauBrigand.cs
+++ Assets/Scripts/VaisseauBrigand.cs
@@ -6,25 +6,27 @@ using UnityEngine.AI;
 public class VaisseauBrigand : MonoBehaviour
 {
     public float movementSpeed = 1.5f, rotationSpeed = 100f;
+    // Doté de plusieurs points de vie
     public int vieMax = 150;
     private Transform joueur;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Vie vie = new Vie(vieMax);
         joueur = GameObject.Find("PlayerShip").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Cherche constamment à se diriger vers la position du joueur
         transform.LookAt(joueur);
         transform.position += transform.forward * movementSpeed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Subira des dommages s'il entre en collision avec un astéroide ou joueur (fonctionne seulement avec astéroide)
         if (other.gameObject.CompareTag("Asteroid"))
         {
             Debug.Log("Collision Asteroid");
