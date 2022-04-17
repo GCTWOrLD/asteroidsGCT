4b32096e9a907e16a88cd5b5c411aa580f8fa50d
GCTWOrLD <gabriel.caron.tawil@gmail.com>
2022-04-17 06:11:05 -0400

    Collisions entre joueur/aliens/missiles + points associés aux aliens

--- Assets/Scripts/VaisseauBrigand.cs
+++ Assets/Scripts/VaisseauBrigand.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class VaisseauBrigand : MonoBehaviour
 {
     public float movementSpeed = 1.5f, rotationSpeed = 100f;
     // Doté de plusieurs points de vie
-    public int vieMax = 150;
+    public int vieMax = 400;
+    public int nbPoints = 10;
+
     private Transform joueur;
 
     // Start is called before the first frame update
@@ -21,7 +22,7 @@ public class VaisseauBrigand : MonoBehaviour
     {
         // Cherche constamment à se diriger vers la position du joueur
         transform.LookAt(joueur);
-        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        //transform.position += transform.forward * movementSpeed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,22 +30,22 @@ public class VaisseauBrigand : MonoBehaviour
         // Subira des dommages s'il entre en collision avec un astéroide ou joueur (fonctionne seulement avec astéroide)
         if (other.gameObject.CompareTag("Asteroid"))
         {
-            Debug.Log("Collision Asteroid");
             Destroy(other.gameObject);
             vieMax -= 25;
-            if (vieMax == 0)
-            {
-                Destroy(gameObject);
-            }
         }
         else if (other.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Collision Player");
             vieMax -= 25;
-            if (vieMax == 0)
-            {
-                Destroy(gameObject);
-            }
+        }
+        else if (other.gameObject.CompareTag("Missle"))
+        {
+            vieMax -= 50;
+        }
+
+        if (vieMax <= 0)
+        {
+            Destroy(gameObject);
+            ScoreManager.Instance.AddScore(nbPoints);
         }
     }
 }
